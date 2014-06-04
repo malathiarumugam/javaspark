ccbca23e0aeff71b0848bfef62f4d74963ee9cc6
Victor Palma <palma.victor@gmail.com>
2014-06-04 12:51:51 -0500

    Minor fixes, and added sqlite dependcies for future functionality

---
+++ src/main/java/com/devx/testapp/ApiModule.java
@@ -0,0 +1,12 @@
+package com.devx.testapp;
+
+import dagger.Module;
+import dagger.Provides;
+
+import javax.inject.Singleton;
+
+@Module(entryPoints = ApiController.class)
+public class ApiModule {
+    // This will hold all our methods the REST "service"
+
+}
